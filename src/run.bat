6e0c34fac110df80be2a6357710db11c6cd921d2
Jay Flowers <jay.flowers@gmail.com>
2012-06-03 11:10:12 -0400

    install ruby and chef

--- src/run.bat
+++ src/run.bat
@@ -1,1 +1,3 @@
-notepad.exe
+IF NOT EXIST C:\Ruby193\bin\ruby.exe rubyinstaller-1.9.3-p194.exe /silent
+::gem update --system
+IF NOT EXIST C:\opscode\chef\bin\chef-solo.bat msiexec /qb /i chef-client-latest.msi
