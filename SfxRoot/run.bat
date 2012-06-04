c8c17e48fc9c31b66ee47893bd94ead60304ae66
Jay Flowers <jay.flowers@gmail.com>
2012-06-03 20:51:38 -0400

    add chef solo config with a test cookbook

--- SfxRoot/run.bat
+++ SfxRoot/run.bat
@@ -1,3 +1,9 @@
+
+SET PATH=C:\Ruby193\bin;C:\opscode\chef\bin;%PATH%
+
 IF NOT EXIST C:\Ruby193\bin\ruby.exe rubyinstaller-1.9.3-p194.exe /silent
 ::gem update --system
+
 IF NOT EXIST C:\opscode\chef\bin\chef-solo.bat msiexec /qb /i chef-client-latest.msi
+
+chef-solo -c .\Chef\solo.rb
