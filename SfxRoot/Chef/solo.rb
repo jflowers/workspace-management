d1f782a3d150eb6bb10279bd11b7788411de253f
Jay Flowers <jay.flowers@gmail.com>
2012-06-03 21:46:46 -0400

    fixes, its working

--- SfxRoot/Chef/solo.rb
+++ SfxRoot/Chef/solo.rb
@@ -1,4 +1,6 @@
 file_cache_path File.dirname(__FILE__) + "/Chef/Cache"
-cookbook_path File.dirname(__FILE__) + "/Chef/Cookbooks"
-json_attribs "http://10.153.10.173:8082/nexus/service/local/artifact/maven/redirect?r=snapshots&g=com.agilex.chef&a=node&v=1.0-SNAPSHOT&e=json"
-recipe_url "http://10.153.10.173:8082/nexus/service/local/artifact/maven/redirect?r=snapshots&g=com.agilex.chef&a=chef-solo-cookbooks&v=1.0-SNAPSHOT&e=tar.gz"
+cookbooks_path [File.dirname(__FILE__) + "/Chef/Cookbooks"]
+json_attribs "http://10.153.10.173:8082/nexus/service/local/artifact/maven/redirect?r=thirdparty&g=com.agilex.chef&a=node&v=1.0&e=json"
+recipe_url "http://10.153.10.173:8082/nexus/service/local/artifact/maven/redirect?r=thirdparty&g=com.agilex.chef&a=chef-solo-cookbooks&v=1.0&e=tar.gz"
+
+log_location STDOUT
--- SfxRoot/run.bat
+++ SfxRoot/run.bat
@@ -7,3 +7,5 @@ IF NOT EXIST C:\Ruby193\bin\ruby.exe rubyinstaller-1.9.3-p194.exe /silent
 IF NOT EXIST C:\opscode\chef\bin\chef-solo.bat msiexec /qb /i chef-client-latest.msi
 
 chef-solo -c .\Chef\solo.rb
+
+pause
